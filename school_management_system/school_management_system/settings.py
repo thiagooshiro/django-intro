f25dc58ad51196164225c70d9167d373a824ed60
thiagobuscarid <thiago.buscarid@gmail.com>
2024-07-20 12:19:18 -0300

    avoiding cors

--- school_management_system/school_management_system/settings.py
+++ school_management_system/school_management_system/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'students'
 ]
 
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'school_management_system.urls'
@@ -134,3 +136,9 @@ DATABASES = {
     }
 }
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    # Adicione outros domínios se necessário
+]
